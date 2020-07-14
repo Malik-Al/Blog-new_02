6ce7eec61574b256c2fc0e8a55e96ab4047950ab
Malik <imenov-a@mail.ru>
2020-07-15 05:25:16 +0600

    Добавлено представления ArticleView для главное страницы на базе TemplateView.

--- source/main/urls.py
+++ source/main/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import IndexView, article_create_view, article_view, article_update_view, article_delete_view
+from webapp.views import IndexView, article_create_view, ArticleView, article_update_view, article_delete_view
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
     path('article/create/', article_create_view, name='article_add'),
-    path('article/<int:pk>', article_view, name='article_view'),
+    path('article/<int:pk>', ArticleView.as_view(), name='article_view'),
     path('article/<int:pk>/edit/', article_update_view, name='article_update'),
     path('article/<int:pk>/delete/', article_delete_view, name='article_delete'),
     path('admin/', admin.site.urls),
