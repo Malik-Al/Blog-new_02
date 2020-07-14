912424306cced6543fd7696d16fa935293d11a5d
Malik <imenov-a@mail.ru>
2020-07-15 01:33:26 +0600

    Поднял проект Blog.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from webapp.models import Article
 
-# Register your models here.
+
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'author', 'category', 'created_at']
+    list_filter = ['author', 'category']
+    search_fields = ['title', 'text']
+    fields = ['title', 'author', 'text',  'created_at', 'updated_at', 'category']
+    readonly_fields = ['created_at', 'updated_at']
+
+admin.site.register(Article, ArticleAdmin)
