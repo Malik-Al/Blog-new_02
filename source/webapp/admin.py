28e0405157128ba4c9389678e383f860f55c7684
Malik <imenov-a@mail.ru>
2020-07-16 01:43:16 +0600

    Добавилено incline Comment admin и articles.

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -2,12 +2,20 @@ from django.contrib import admin
 from webapp.models import Article, Comment, Category
 
 
+class CommentAdmin(admin.TabularInline):
+    model = Comment
+    fields = ['author', 'text']
+    readonly_fields = ['author', 'text']
+    extra = 0
+
+
 class ArticleAdmin(admin.ModelAdmin):
     list_display = ['id', 'title', 'author', 'created_at']
     list_filter = ['author', 'category']
     search_fields = ['title', 'text']
-    fields = ['title', 'author', 'text', 'category', 'created_at', 'updated_at']
+    exclude = []
     readonly_fields = ['created_at', 'updated_at']
+    inlines = [CommentAdmin]
 
 
 admin.site.register(Article, ArticleAdmin)
