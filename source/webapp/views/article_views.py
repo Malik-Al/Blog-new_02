a0dba6f3042e8cb84544d86bbb0ef270b000d33f
Malik <imenov-a@mail.ru>
2020-07-22 01:51:01 +0600

    Сделал Рефакторинг проекта.

---
+++ source/webapp/views/article_views.py
@@ -0,0 +1,95 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+
+from webapp.forms import ArticleForm
+from webapp.models import Article
+from django.views.generic import TemplateView
+
+
+
+class IndexView(TemplateView):
+    template_name = 'article/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['articles'] = Article.objects.all()
+        return context
+
+
+
+class ArticleView(TemplateView):
+    template_name = 'article/article.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        article_pk = kwargs.get('pk')
+        context['article'] = get_object_or_404(Article, pk=article_pk)
+        return context
+
+
+
+
+class  ArticleCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = ArticleForm()
+        return render(request, 'article/create.html', context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article = Article.objects.create(
+                title=form.cleaned_data['title'],
+                author=form.cleaned_data['author'],
+                text=form.cleaned_data['text'],
+                category=form.cleaned_data['category']
+            )
+            return redirect('article_view', pk=article.pk)
+        else:
+            return render(request, 'article/create.html', context={'form': form})
+
+
+
+class ArticleUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        article_pk = kwargs.get('pk')
+        article = get_object_or_404(Article, pk=article_pk)
+        form = ArticleForm(data={
+            'title': article.title,
+            'text': article.text,
+            'author': article.author,
+            'category': article.category_id
+
+        })
+        return render(request, 'article/update.html', context={'form': form, 'article': article})
+
+    def post(self, request, *args, **kwargs):
+        article_pk = kwargs.get('pk')
+        article = get_object_or_404(Article, pk=article_pk)
+        form = ArticleForm(data=request.POST)
+        if form.is_valid():
+            article.title = form.cleaned_data['title']
+            article.text = form.cleaned_data['text']
+            article.author = form.cleaned_data['author']
+            article.category = form.cleaned_data['category']
+            article.save()
+            return redirect('article_view', pk=article.pk)
+        else:
+            return render(request, 'article/update.html', context={'form': form, 'article': article})
+
+
+
+class ArticleDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        article_pk = kwargs.get('pk')
+        article = get_object_or_404(Article, pk=article_pk)
+        return render(request, 'article/delete.html', context={'article': article})
+
+    def post(self, request, *args, **kwargs):
+        article_pk = kwargs.get('pk')
+        article = get_object_or_404(Article, pk=article_pk)
+        article.delete()
+        return redirect('index')
+
+
+
+
---
+++ source/webapp/views/comment_views.py
@@ -0,0 +1,74 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.views import View
+
+from webapp.forms import CommentForm
+from webapp.models import Comment
+from django.views.generic import TemplateView
+
+
+
+class CommentIndexView(TemplateView):
+    template_name = 'comment/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['comments'] = Comment.objects.all()
+        return context
+
+
+
+
+
+class CommentCreateView(View):
+    def get(self, request, *args, **kwargs):
+        form = CommentForm()
+        return render(request, 'comment/create.html', context={'form': form})
+
+    def post(self, request, *args, **kwargs):
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            comment = Comment.objects.create(
+                author=form.cleaned_data['author'],
+                text=form.cleaned_data['text'],
+                article=form.cleaned_data['article'],
+
+            )
+            return redirect('comment_index', pk=comment.article.pk)
+        else:
+            return render(request, 'comment/create.html', context={'form': form})
+
+
+
+class CommentUpdateView(View):
+    def get(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs.get('pk'))
+        form = CommentForm(data={
+            'author': comment.author,
+            'text': comment.text,
+            'article': comment.article
+
+        })
+        return render(request, 'comment/update.html', context={'form': form, 'comment': comment})
+
+    def post(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs.get('pk'))
+        form = CommentForm(data=request.POST)
+        if form.is_valid():
+            comment.author = form.cleaned_data['author']
+            comment.text = form.cleaned_data['text']
+            comment.article = form.cleaned_data['article']
+            comment.save()
+            return redirect('comment_index')
+        else:
+            return render(request, 'comment/update.html', context={'form': form, 'comment': comment})
+
+
+
+class CommentDeleteView(View):
+    def get(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs.get('pk'))
+        return render(request, 'comment/delete.html', context={'comment': comment})
+    def post(self, request, *args, **kwargs):
+        comment = get_object_or_404(Comment, pk=kwargs.get('pk'))
+        comment.delete()
+        return redirect('comment_index')
