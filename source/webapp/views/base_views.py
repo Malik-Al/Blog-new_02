82f96ca51d44e4262f9619f99013cd67df1ffcf1
Malik <imenov-a@mail.ru>
2020-07-22 14:59:22 +0600

    Убрал не нужный код

--- source/webapp/views/base_views.py
+++ source/webapp/views/base_views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import get_object_or_404
 from django.views.generic import TemplateView
 
 
@@ -16,12 +15,3 @@ class ListView(TemplateView):
 
 
 
-class DetailView(TemplateView):
-    context_key = 'objects'
-    model = None
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        pk = kwargs.get('pk')
-        context[self.context_key] = get_object_or_404(self.model, pk=pk)
-        return context
