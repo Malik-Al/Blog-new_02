9e8b70e5a34d6726529b9ee57949b176573f62fe
Malik <imenov-a@mail.ru>
2020-08-15 02:47:53 +0600

    Добавлено просмотр профиля пользователи

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.urls import reverse
+from django.views.generic import DetailView
+
 from accounts.forms import UserCreationForm
 from django.contrib.auth.models import User
 from accounts.models import Token
@@ -70,8 +72,10 @@ def user_activate(request):
 
 
 
-
-
+class UserDetailView(DetailView):
+    model = User
+    template_name = 'user_detail.html'
+    context_object_name = 'user_obj'
 
 
 
